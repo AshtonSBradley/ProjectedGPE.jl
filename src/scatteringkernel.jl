27b8685ba4244b41999c0ac7a20ceee31bc90453
AshtonSBradley <ashton.bradley@otago.ac.nz>
2017-05-27 14:51:05 +1200

    fix absolute value
    abs(k) needed fix in 3D

--- src/scatteringkernel.jl
+++ src/scatteringkernel.jl
@@ -18,7 +18,7 @@ function scatteringkernel(k,σ=1)
 if ndims(k) == 1
   return erfcx(abs(k)*σ/sqrt(2))/sqrt(8*π*σ^2)
 elseif ndims(k) == 2
-  return exp((k*σ/2)^2)*besselk(0,(k*σ/2)^2)/(2*π)
+  return exp((abs(k)*σ/2)^2)*besselk(0,(abs(k)*σ/2)^2)/(2*π)
 else
  return 1./abs(k)
 end
