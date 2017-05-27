bff78c3809a68c0a79779f55066b900a7dca3f34
AshtonSBradley <ashton.bradley@otago.ac.nz>
2017-05-27 18:46:03 +1200

    add includes

--- src/ProjectedGPE.jl
+++ src/ProjectedGPE.jl
@@ -10,8 +10,10 @@ abstract cField
 include("eigmat.jl")
 include("nfieldtrans.jl")
 include("unwrap.jl") #putting this here for now (=> package VortexDetect.jl)
+include("scatteringkernel.jl")
+include("findvortices.jl")
 
-export eigmat, nfieldtrans, unwrap, scatteringkernel, findvortices 
+export eigmat, nfieldtrans, unwrap, scatteringkernel, findvortices
 
 
 end # module
