aed1d2c3ec83341f83e4318c1e9a81bbc4dace3e
AshtonSBradley <ashton.bradley@otago.ac.nz>
2017-05-29 19:37:43 +1200

    Appveyor/Tavis
    First read through and edits of CI files

--- docs/make.jl
+++ docs/make.jl
@@ -1,16 +1,15 @@
-using Documenter
-using ProjectedGPE
+using Documenter, ProjectedGPE
 
 makedocs(
-    modules = [ProjectedGPE]
+    modules = [ProjectedGPE],
+    doctest=true
 )
 
-#=
 deploydocs(deps   = Deps.pip("mkdocs", "python-markdown-math"),
-    repo = "github.com/AshtonSBradley/ProjectedGPE.git",
+    repo = "github.com/AshtonSBradley/ProjectedGPE.jl.git",
     julia  = "0.5.2",
     osname = "macos")
-=#
+
 
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
--- docs/src/index.md
+++ docs/src/index.md
@@ -1,3 +1,5 @@
 # ProjectedGPE.jl
 
 Documentation for ProjectedGPE.jl
+
+A simple package for setup, evolution, and analysis of ProjectedGPE simulations
