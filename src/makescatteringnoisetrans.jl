80c1752e90ddb5171612cddd978b39e0a485d0ab
AshtonSBradley <ashton.bradley@otago.ac.nz>
2017-06-02 19:09:01 +1200

    Noise checks
    found 2field-vs 4 field problem

--- src/makescatteringnoisetrans.jl
+++ src/makescatteringnoisetrans.jl
@@ -1,7 +1,7 @@
 function makescatteringnoisetrans(basis,M,Nk=M,ω=1)
 #Noise transform: 2-field quadrature to go from basis to k-space
-kn,wkn,Tkn = nfieldtrans(basis,Nk,4,1/ω)
-Tkn = diagm((-im).^(0:2M-1))*eigmat(basis,M,kn,1/ω)
+kn,wkn,Tkn = nfieldtrans(basis,Nk,2,1/ω)
+Tkn = diagm((-im).^(0:M-1))*eigmat(basis,M,kn,1/ω)
 
 #Noise: position space roots and weights for 3-field quadrature
 xn,wxn,Txn = nfieldtrans(basis,M,3,ω)
