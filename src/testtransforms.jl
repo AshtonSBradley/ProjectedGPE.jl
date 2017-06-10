7dd219c4efe9e77113912b54ac52222a5ccaf083
AshtonSBradley <ashton.bradley@otago.ac.nz>
2017-06-10 15:07:53 +1200

    More testing transforms

--- src/testtransforms.jl
+++ src/testtransforms.jl
@@ -1,5 +1,5 @@
 using ProjectedGPE, ApproxFun, FastGaussQuadrature, PyPlot, Combinatorics
-M=100
+M=350
 tol = 1e-14
 #c=Vector{BigFloat}(randn(M))+im*Vector{BigFloat}(randn(M))
 #c=randn(M)+im*randn(M)
@@ -15,14 +15,15 @@ n=0:BigFloat(M)-1;
 Hnorm = sqrt(BigFloat(π))*BigFloat(2).^n.*factorial.(n)
 
 #Inserting norm.
-f=Fun(Hermite(),c./sqrt(Hnorm));
-
+f=Fun(Hermite(),c./sqrt(Hnorm))
+f2=Fun(GaussWeight(Hermite()),c./sqrt(Hnorm))
 x=linspace(-1.5sqrt(2M),1.5sqrt(2M),1000)
 plot(x,abs(f.(x)).^2.*exp.(-x.^2))
 
 x,w=gausshermite(M)
 
 N2=sum(w.*abs(f.(x)).^2)
+N3=sum(w.*abs(f2.(x).*exp(x.^2)).^2)
 
 Float64(N2)
 
@@ -32,7 +33,7 @@ abs((N1-N2)/N1)
 3*M*tol
 
 #block to create T matrix
-M=100
+M=10
 x,w=gausshermite(M)
 T=zeros(x*ones(1,M))
 n=0:BigFloat(M)-1;
@@ -41,8 +42,10 @@ Hnorm = sqrt(BigFloat(π))*BigFloat(2).^n.*factorial.(n)
 for j = 0:M-1
   c=zeros(M)
   c[j+1]=1.
-  Hn=Fun(Hermite(),c./sqrt(Hnorm));
-  T[:,j+1]=Hn.(x).*exp(-x.^2/2)
+  #Hn=Fun(Hermite(),c./sqrt(Hnorm));
+  #T[:,j+1]=Hn.(x).*exp(-x.^2/2)
+  Hn=Fun(GaussWeight(Hermite()),c)
+  T[:,j+1]=Hn.(x).*exp(x.^2/2)
 end
 
 x1,y1,T1=nfieldtrans("Hermite",M,2,2)
@@ -78,8 +81,15 @@ c[end]=BigFloat(1.)
 N1 = sum(abs(c).^2)
 L=Fun(Laguerre(α),c./sqrt(Lnorm))
 
+
+
+
+L2=Fun(WeightedLaguerre(α),c./sqrt(Lnorm))
 N2=sum(w.*abs(L.(sqrt(ω)*x)).^2)
 
+
+
+N3=sum(w.*abs(L2.(sqrt(ω)*x).*exp(sqrt(ω)*x/2)./(sqrt(ω)x).^(α/2)).^2)
 x=linspace(0,20sqrt(M),1000)
 plot(x,abs(L.(x)).^2.*exp.(-x).*x.^α)
 
@@ -87,12 +97,14 @@ Lnorm
 
 #------------------------------------
 #test Laguerre
-M=21
-α = 1.
-ω = 2.
-x,w=gausslaguerre(M,α)
+M=27
+α = 0.
+ω = 1.
+#x,w=gausslaguerre(M,α)
 
-T = eigmat("Laguerre",M,x/ω,ω,α)
+#T = eigmat("Laguerre",M,x/ω,ω,α)
+
+x,w,T=nfieldtrans("Laguerre",M,2,ω,α)
 #norm test
 
 c=zeros(M);
@@ -102,4 +114,26 @@ N1 = sum(abs(c).^2)
 Ln = T*c
 
 
-N2=sum(w.*abs(Ln).^2.*exp(x)./(x).^α)
+N2=sum(w.*abs(Ln).^2)
+f1 = Fun(GaussWeight(Hermite()),[1])
+
+#------------------------------------
+#test Hermite
+M=21
+
+ω = 10
+#x,w=gausslaguerre(M,α)
+
+#T = eigmat("Laguerre",M,x/ω,ω,α)
+
+x,w,T=nfieldtrans("Hermite",M,2,ω)
+#norm test
+
+c=zeros(M);
+c[end]=1.
+N1 = sum(abs(c).^2)
+
+Hn = T*c
+
+
+N2=sum(w.*abs(Hn).^2)
