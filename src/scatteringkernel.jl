90207c304b99358aa33fb4a1a7136ad1bab00e86
Ashton Bradley <ashton.bradley@gmail.com>
2018-01-10 20:41:50 +1300

    update docs for scattering kernel

--- src/scatteringkernel.jl
+++ src/scatteringkernel.jl
@@ -1,14 +1,14 @@
 """
 
-`scatteringkernel(k,σ)`
-Evaluates the scattering kernel in momentum space and returns on the same `k`-grid.
+`kernel = scatteringkernel(k,σ)`
+
+Construct the scattering kernel in momentum space and returns on the same `k`-grid.
 This is used to construct both the energy-damping term, and noise in the energy-damped
 stochastic projected Gross-Pitaevskii equation.
 
-## Arguments
-* `k` - the momentum-space grid in where scattering is to be evaluated
-* `σ` - width parameter for `k`-space degrees of freedom in the case of N=1,2 spatial dimensions.
+`k` the momentum-space grid in where scattering is to be evaluated.
 
+`σ` width parameter for `k`-space degrees of freedom in the case of N=1,2 spatial dimensions.
 
 Low dimensional SPGPE scattering kernel functions:
 [Low-D stochastic projected GPE](https://arxiv.org/abs/1507.02023)
