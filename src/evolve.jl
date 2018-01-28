529d89973c7e2e16b316d777f01d887e69db09dd
Ashton Bradley <ashton.bradley@gmail.com>
2018-01-28 22:58:22 +1300

    prune methods

--- src/evolve.jl
+++ src/evolve.jl
@@ -1,48 +1,66 @@
-function evolve()
-  #definition of the C-region
+function evolve2()
+siminfo = Params()
 
-  #system params
-  ωx = 2*pi
-  t0 = 1/ωx
-  g = 0.1
-  Γ̄ = 0.07
-  M̄ = 0.0
+#== start template ==#
+#fundamental constants/units
+  ħ = 1.0545718e-34
+  kB = 1.38064852e-23
+  amu = 1.660339040e-27
+  Bohr = 5.29e-11
+#Rb87 mass and scattering length
+  m = 86.909180527*amu
+  as = 100*Bohr
+#trap frequencies
+  ωx = 2π
+  ωy = 4ωx
+  ωz = 0.
+#choice of time, length, energy units
+  t0 = 1.0/ωy
+  x0 = sqrt(ħ*t0/m)
+  E0 = ħ/t0
+#interactions
+  #g  = (4*pi*ħ^2*as/m)*x0^3/E0 #dimensionless 3D
+  g = 0.1 #test 2D
+#damping parameters (dimensionless)
+  Γ̄  = 0.1
+  M̄  = 0.0
+#chemical potential (dimensionless)
+  μ  = 12.0
+#time evolution parameters
+  ti = 0.0
+  tf = 2.0/Γ̄  #evolve for 2 damping times
+  Nt = 50
+  t = collect(linspace(ti,tf,Nt))
+  dt = 0.01π/μ #integrate step size [ - should have dt ≪ 2π/μ]
+#== end template ==#
 
-  μ = 20.0
+  @pack siminfo = ωx,ωy,ωz,Γ̄,M̄,g,t0,E0,x0,μ,ti,tf,Nt,t,dt
 
-  #Time grid
-  ti = 0.0              #initial time
-  tf = 80*t0          #final time
-  Nt = 40             #size of time vector
-  t  = collect(linspace(ti,tf,Nt))
-
-  dt = 0.1*t0    #integrate step size [ - should have dt ≪ 2π/μ]
-  @pack siminfo = ωx,ωy,ωz,Γ̄,M̄,g,t0,μ,ti,tf,Nt,t,dt
   #Initialize CField
   basis = "Hermite"
-  ecut = 30 #units of ωx
-  Ω = [1.]
+  ecut = 30 #units of ħ*ωy
+  Ω = [ωx; ωy]*t0
   cinfo = makecinfo(ecut,Ω,basis)
-  @unpack en,P,M = cinfo; Mx=M[1]
-  x,wx,Tx = maketransinfo(M,Ω,basis)
-
+  @unpack en,P,M = cinfo ;Mx=M[1];My=M[2]
+  x,wx,Tx,y,wy,Ty = makealltrans(M,Ω,n=4,basis=basis)
+  Wxy = wx.*wy'
 #test transform
-  c0   = randn(Mx)+im*randn(Mx);
-  ψ0   = Tx*c0
-  ψ    = Tx*c0  #a field to write to in place if needed
+  c0   = randn(Mx,My)+im*randn(Mx,My);
+  c0   = P.*c0;   #Project
+  ψ0   = Tx*c0*Ty'
+  ψ    = Tx*c0*Ty' #a field to write to in place if needed
 
 #PGPE nonlinearity 1D
 #out of place
 function nlin(c)
-    ψ = Tx*c
-    Tx'*(wx.*abs.(ψ).^2.*ψ)
+    ψ = Tx*c*Ty'
+    Tx'*(Wxy.*abs.(ψ).^2.*ψ)*Ty
 end
 
 function nlin!(c,dc)
-    ψ = Tx*c
-    dc.= Tx'*(wx.*abs.(ψ).^2.*ψ)
+    ψ = Tx*c*Ty'
+    dc.= Tx'*(Wxy.*abs.(ψ).^2.*ψ)*Ty
 end
-
 
 #dPGPE in reservoir frame
 #out of place
@@ -52,12 +70,13 @@ end
 
 #in place
 function Lgp!(t,c,dc)
-    dc.= nlin!(c,dc)
+    dc = nlin!(c,dc)
     dc.= -im*(1-im*Γ̄)*((en - μ).*c .+ g*dc)
+    dc.= P.*dc
 end
 
-c0    = randn(Mx) + im*randn(Mx); #create new random initial state
-tspan = (ti,tf)
+c0    = randn(Mx,My) + im*randn(Mx,My); #create new random initial state
+tspan = (t[1],t[end])
 prob = ODEProblem(Lgp!,c0,tspan)
 alg = DP5()
 #abstol = 1e-3!
@@ -65,5 +84,5 @@ alg = DP5()
 println("Started evolution ...")
 @time sol = solve(prob,alg,dt=dt,saveat=t,alg_hints=[:stiff],save_everystep=false,dense=false);
 println("... Finished.")
-return cinfo,sol
+return siminfo,cinfo,sol
 end
