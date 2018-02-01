bd5b78527c7412b2e2523599b21a32c579f90270
Ashton Bradley <ashton.bradley@gmail.com>
2018-02-01 19:26:55 +1300

    update xgrid2c

--- src/xgrid2c.jl
+++ src/xgrid2c.jl
@@ -4,12 +4,12 @@
 
     function xgrid2c(ψ::Array{Complex{Float64},2},x,y,cinfo::ProjectedGPE.Cinfo)
         Mx,My = cinfo.M
-        Δx = x[2]-x[1];Δy = y[2]-y[1]
+        dx = x[2]-x[1];dy = y[2]-y[1]
         Tx = eigmat(Mx,x)
         Ty = eigmat(My,y)
-        return Tx'*(ψ*Ty)*Δx*Δy
+        return Tx'*(ψ*Ty)*dx*dy
     end
 
     function x2c(ψ::Array{Complex{Float64},3},cinfo::ProjectedGPE.Cinfo)
-        error("impliment")
+        error("implement")
     end
