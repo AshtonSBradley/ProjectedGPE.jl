858a893666c8b88fcfc42a0d0d9b8ee9b59b5c9a
Ashton Bradley <ashton.bradley@gmail.com>
2018-07-01 21:50:22 +1000

    small formatting

--- src/maketinfoplot.jl
+++ src/maketinfoplot.jl
@@ -1,12 +1,11 @@
 function maketinfoplot(cinfo,x,y)
 tinfo = Tinfo()
 
-@unpack en, P, M, Ω, basis = cinfo
+@unpack en,P,M,Ω,basis = cinfo
 ωx,ωy=Ω; Mx,My=M;
 Tx = eigmat(Mx,x,ω=ωx/ωy)
 Ty = eigmat(My,y,ω=ωy/ωy)
-
-@pack tinfo = Tx, Ty
+@pack tinfo = Tx,Ty
 
 return tinfo
 end
