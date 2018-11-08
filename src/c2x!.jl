16623c1e00bbbae73e7448bd9b38f5b7e97979e9
Ashton Bradley <ashton.bradley@gmail.com>
2018-11-08 14:11:08 +1300

    small change of notation

--- src/c2x!.jl
+++ src/c2x!.jl
@@ -8,15 +8,16 @@ end
 
 function c2x!(ψ::Array{Complex{Float64},3},c::Array{Complex{Float64},3},tinfo::ProjectedGPE.Tinfo)
     sa = size(c);sx = size(tinfo.Tx);sy = size(tinfo.Ty);sz = size(tinfo.Tz)
+    acyc = (2,3,1)
     Ax = reshape(c,(sa[1],sa[2]*sa[3]))
     Ax = reshape(tinfo.Tx*Ax,(sx[1],sa[2],sa[3]))
-    Ax = permutedims(Ax,(2,3,1));sa = size(Ax)
+    Ax = permutedims(Ax,acyc);sa = size(Ax)
 
     Ay = reshape(Ax,(sa[1],sa[2]*sa[3]))
     Ay = reshape(tinfo.Ty*Ay,(sy[1],sa[2],sa[3]))
-    Ay = permutedims(Ay,(2,3,1));sa = size(Ay)
+    Ay = permutedims(Ay,acyc);sa = size(Ay)
 
     Az = reshape(Ay,(sa[1],sa[2]*sa[3]))
     Az = reshape(tinfo.Tz*Az,(sz[1],sa[2],sa[3]))
-    ψ = permutedims(Az,(2,3,1))
+    ψ = permutedims(Az,acyc)
 end
